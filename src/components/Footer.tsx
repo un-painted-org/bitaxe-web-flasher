352e7d56256421068838b92009b00cb1285a0209
WantClue <Lars_Naether@hotmail.de>
2025-01-13 11:01:53 +0100

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -43,7 +43,7 @@ export default function Footer() {
       )}
 
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-gray-500">© 2024 Bitaxe Web Flasher. All rights reserved.</p>
+        <p className="text-xs text-gray-500">© 2025 Bitaxe Web Flasher. All rights reserved.</p>
         <Link className="text-xs hover:underline underline-offset-4" href="https://wantclue.de">
           Maintained by WantClue
         </Link>
